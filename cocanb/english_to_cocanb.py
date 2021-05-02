a19f115793c3d1f3bd9ce88b3fb2bda02bca4cbb
Woojin Son <>
2021-05-02 03:20:55 +0000

    more stuff

--- cocanb/english_to_cocanb.py
+++ cocanb/english_to_cocanb.py
@@ -1,8 +1,27 @@
-def english_to_cocanb(sentence):
+# by nonka
+
+# original just in case i fuck up
+# def toc(sentence):
+#     initial = sentence.split()
+#     additional = ""
+#     for i in initial:
+#         additional += i[-1] + chr(ord('`') + len(i))
+#         initial[initial.index(i)] = i[:-1]
+#     final = "".join(initial) + "non" + additional
+#     return final
+
+def toc(sentence):
     initial = sentence.split()
     additional = ""
+    nums = []
     for i in initial:
-        additional += i[-1] + chr(ord('`') + len(i))
-        initial[initial.index(i)] = i[:-1]
+        if i.isnumeric():
+            nums.append(i)
+            i = "<num>"
+    for i in initial:
+        if (i[0] != "<" and i[-1] != ">"):
+            additional += i[-1] + ("å" * (len(i) // 26)) + chr(ord('`') + (len(i) % 26))
+            initial[initial.index(i)] = i[:-1]
     final = "".join(initial) + "non" + additional
+    final = final.replace("<", " <").replace(">", "> ")
     return final
---
+++ main.py
@@ -0,0 +1,5 @@
+# testing file. delete when ready
+
+import cocanb
+
+print(cocanb.english_to_cocanb("I am testing 4 of these \"quoty things\" and I am hoping it works."))
